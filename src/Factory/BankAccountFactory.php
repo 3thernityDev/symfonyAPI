eabb1f41e2b63401b716045077d0c981f7a3dcfb
3thernitydev <3thernitydev@gmail.com>
2025-01-15 11:09:16 +0400

    add BankAccountFactory

---
+++ src/Factory/BankAccountFactory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\BankAccount;
+use App\Repository\BankAccountRepository;
+use Doctrine\ORM\EntityRepository;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
+
+/**
+ * @extends PersistentProxyObjectFactory<BankAccount>
+ */
+final class BankAccountFactory extends PersistentProxyObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct() {}
+
+    public static function class(): string
+    {
+        return BankAccount::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+            'accountNumber' => self::faker()->randomNumber(),
+            'balance' => self::faker()->randomNumber(),
+            'currency' => self::faker()->randomElement(['USD', 'EUR', 'GBP']),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(BankAccount $bankAccount): void {})
+        ;
+    }
+}
